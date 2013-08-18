676900c577b68291c576aea0d06b5d8d0c502ac1
ashwath26 <ashwath26@yahoo.com>
2013-08-18 12:49:41 -0700

    Project folder structure
    Adding new folder structure  for the project and sample hibernate
    files.

---
+++ src/main/java/edu/uic/cs/postalservice/entity/Employee.java
@@ -0,0 +1,48 @@
+package java.edu.uic.cs.postalservice.entity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ashwath
+ * Date: 8/18/13
+ * Time: 2:39 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Employee {
+
+    private int id;
+    private String fname;
+    private String lname;
+    private int salary;
+
+    public Employee(){};
+    public Employee(String fname, String lname, int salary)
+    {
+        this.fname = fname;
+        this.lname = lname;
+        this.salary = salary;
+    }
+    public void setSalary(int salary){
+        this.salary = salary;
+    }
+    public void setFname(String fname){
+        this.fname = fname;
+    }
+    public void setLname(String lname){
+        this.lname = lname;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getId(){
+        return id;
+    }
+    public int getSalary(){
+        return salary;
+    }
+    public String getFname(){
+        return fname;
+    }
+    public String getLname(){
+        return lname;
+    }
+}
